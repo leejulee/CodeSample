5a458cd4489a1ed0c24a878ece9eb342fade00db
leoli <cvsf04456@yahoo.com.tw>
2015-05-01 15:35:31 +0800

    Add Ckeditor,Ckfinder Example

--- MvcProject.Web/Controllers/HomeController.cs
+++ MvcProject.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using MvcProject.Web.App_GlobalResources;
+﻿using Microsoft.Security.Application;
+using MvcProject.Web.App_GlobalResources;
 using MVCProject.Logic;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,20 @@ namespace MvcProject.Web.Controllers
         public ActionResult Index()
         {
             return View(MVCProjectBusinessFactory.LogBusiness.GetAllData());
+        }
+
+        public ActionResult HtmlEditor()
+        {
+            ViewBag.UserEditor = "<img alt=\"\" src=\"/Content/EditorImages/images/contact-01.png\" style=\"height:80px; width:80px\" />\r\n<ul>\r\n\t<li>\r\n\t<h1>這是<strong>一筆</strong>測試資料，</h1>\r\n\t</li>\r\n</ul>\r\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MTK&nbsp;8-core&nbsp;processor&nbsp;available&nbsp;at&nbsp;1.4GHz&nbsp;or&nbsp;1.7GHz,78%&nbsp;higher&nbsp;processing&nbsp;performance&nbsp;than&nbsp;its&nbsp;predecessor";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]//若為屬性可用[AllowHtml]
+        public ActionResult HtmlEditor(string UserEditor)
+        {
+            ViewBag.UserEditor = Sanitizer.GetSafeHtmlFragment(UserEditor);
+            return View();
         }
     }
 }
